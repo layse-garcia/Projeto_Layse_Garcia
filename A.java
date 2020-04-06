c7dd2504fdc244e19fdd6247d27d0b87b4a5fe07
layse-garcia <63252079+layse-garcia@users.noreply.github.com>
2020-04-06 18:41:35 -0300

    Add files via upload
    Classes A e B criadas. Primeira etapa do Trabalho.

---
+++ A.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class A{
+	int a1;
+	float a2;
+	
+	Scanner entrada = new Scanner(System.in);
+	
+	public void setA1(){
+		a1 = entrada.nextInt();
+	}
+	
+	public void setA2(){
+		a2 = entrada.nextFloat();
+	}
+	
+	public int getA1(){
+		return a1;
+	}
+	
+	public float getA2(){
+		return a2;
+	}
+	
+	public void MA1(){
+		System.out.println("MA1");
+	}
+	
+	public void MA2(){
+		System.out.println("MA2");
+	}
+	
+}
---
+++ B.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class A{
+	int b1;
+	float b2;
+	
+	Scanner entrada = new Scanner(System.in);
+	
+	public void setB1(){
+		b1 = entrada.nextInt();
+	}
+	
+	public void setB2(){
+		b2 = entrada.nextFloat();
+	}
+	
+	public int getB1(){
+		return b1;
+	}
+	
+	public float getB2(){
+		return b2;
+	}
+	
+		public void MB1(){
+		System.out.println("MB1");
+	}
+	
+	public void MB2(){
+		System.out.println("MB2");
+	}
+	
+}
