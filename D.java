211e3d39e6f2112d6f4b938bcf06d8f39afda369
Layse Garcia <layse.garcia@estudante.ufla.br>
2020-04-07 11:10:18 -0300

    Criacao da Classe D, com metodo MD1 e MD2

---
+++ D.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class D{
+	Double d1;
+	int d2;
+	
+	Scanner entrada = new Scanner(System.in);
+	
+	public void setD1(){
+		d1 = entrada.nextDouble();
+	}
+	
+	public void setD2(){
+		d2 = entrada.nextInt();
+	}
+	
+	public double getD1(){
+		return d1;
+	}
+	
+	public int getD2(){
+		return d2;
+	}
+	
+	public void MD1(){
+		System.out.println("MD1: Criacao da classe D. Voce esta no metodo MD1");
+	}
+	
+	public void MD2(){
+		System.out.println("MD2: MD1: Criacao da classe D. Voce esta no metodo MD2");
+	}
+	
+}
